83e2f7d84bde0ea3d4b2c20d5c7cc5529da0ad9f
Simer100 <42919299+Simer100@users.noreply.github.com>
2018-10-16 12:28:46 +1300

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.btnload = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnload
+            // 
+            this.btnload.Location = new System.Drawing.Point(671, 126);
+            this.btnload.Name = "btnload";
+            this.btnload.Size = new System.Drawing.Size(75, 23);
+            this.btnload.TabIndex = 1;
+            this.btnload.Text = "Load";
+            this.btnload.UseVisualStyleBackColor = true;
+            this.btnload.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(671, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Spin Chamber";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(671, 301);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Try Again";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(671, 244);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Shoot";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(326, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(335, 50);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Start the roulette";
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(650, 417);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(96, 32);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Exit";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(391, 135);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(144, 50);
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(219, 135);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(151, 123);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::WindowsFormsApp2.Properties.Resources.ezgif_com_resize__1_;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(180, 480);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::WindowsFormsApp2.Properties.Resources._219536_9r9dvp9y_v3;
+            this.ClientSize = new System.Drawing.Size(799, 505);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnload);
+            this.Name = "Form2";
+            this.Text = "PlayGame";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnload;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        //difining integers here
+        public int count = 0, load;
+        public int Spinval;
+
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Image img = Image.FromFile(@"C:\Users\SS077\source\repos\WindowsFormsApp2\Images\ezgif.com-resize 1.gif");
+            pictureBox1.Image = img;
+            //defining the variable for lables
+           
+
+
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+            //Shoot image source and coding
+            //image code
+            Image img = Image.FromFile(@"C:\Users\SS077\source\repos\WindowsFormsApp2\Images\shoot 1.gif");
+            pictureBox3.Image = img;
+            //sound code
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\SS077\source\repos\WindowsFormsApp2\Images\shot.wav");
+            player.Play();
+
+            RouletteGame ShotObj = new RouletteGame();
+            int score = ShotObj.Fire_Bullet(load, Spinval);
+
+           
+
+            if (count == 0 && score == 10)
+            {
+                MessageBox.Show("you won and your score is " + score);
+                button4.Enabled = false;
+            }
+
+            else if (count == 1 && score == 5)
+            {
+                MessageBox.Show("you won and your score is " + score);
+                button4.Enabled = false;
+            }
+            if (count == 1 && score == 0)
+            {
+                MessageBox.Show("Sorry, you losse the game");
+                button4.Enabled = false;
+            }
+            else
+            {
+                count++;
+                Spinval = spinchambervalue(Spinval);
+            }
+        }
+
+        public int spinchambervalue(int spin_val)
+        {
+            if (spin_val == 6)
+            {
+                spin_val = 1;
+
+            }
+            else
+            {
+                spin_val++;
+
+            }
+            return spin_val;
+
+        }
+
+
+
+
+            private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //load image code
+            //Image coding
+            Image img = Image.FromFile(@"C:\Users\SS077\source\repos\WindowsFormsApp2\Images\9b0affdb1c10e17006bc04e391c5ec9b1512793216_full.jpg");
+            pictureBox2.Image = img; 
+            Image img1 = Image.FromFile(@"C:\Users\SS077\source\repos\WindowsFormsApp2\Images\load.gif");
+            pictureBox3.Image = img1;
+            //Sound code(back sound played)
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\SS077\source\repos\WindowsFormsApp2\Images\sound.wav");
+            player.Play();
+            load = 1;
+
+            button2.Enabled = true;
+            btnload.Enabled = false;
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            //Main fixed gif image source
+            Image img = Image.FromFile(@"C:\Users\SS077\source\repos\WindowsFormsApp2\Images\ezgif.com-resize 1.gif");
+            pictureBox1.Image = img;
+        }
+        //Below is the code of picture box 2(face image)
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Below is the code of small picture box named picture box3 (small GIF appears)
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //Exit button code
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Spin image and code
+            //image code
+            Image img = Image.FromFile(@"C:\Users\SS077\source\repos\WindowsFormsApp2\Images\spin.gif");
+            pictureBox3.Image = img;
+            //sound code
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\SS077\source\repos\WindowsFormsApp2\Images\spin sound.wav");
+            player.Play();
+            RouletteGame SpinObj = new RouletteGame();
+            Spinval = SpinObj.Spin_Chamber();
+
+            button4 .Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Try again and reopening the application code
+            RouletteGame try1 = new RouletteGame();
+           try1.try_again();
+
+            (new Form2()).Show();
+            this.Close();
+
+        }
+        //Label 5 in front of scores
+        private void label5_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //lable 6 in front of wins 
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+        //lable 7 in front of looses
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
